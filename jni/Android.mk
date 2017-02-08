e7e91cd0ea695e6d619aa030b48310d5017f754a
MITSUNARI Shigeo <herumi@nifty.com>
2017-02-08 12:48:35 +0900

    use static library for gmp

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,16 +4,15 @@ include $(CLEAR_VARS)
 include ../openssl-android/Android.mk
 
 include $(CLEAR_VARS)
-GMP_WITH_CPLUSPLUS := yes
-include jni/gmp/Android.mk
+include ../gmp-android/Android.mk
+
 LOCAL_PATH := jni
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := mcladt
 LOCAL_SRC_FILES := mcladt.cpp
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../openssl-android/include $(addsuffix /include,$(addprefix $(LOCAL_PATH)/../../../,mcl cybozulib))
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../openssl-android/include $(LOCAL_PATH)/../../gmp-android/include $(addsuffix /include,$(addprefix $(LOCAL_PATH)/../../../,mcl cybozulib))
 LOCAL_CPPFLAGS += -O3 -DNDEBUG -fexceptions -frtti -fPIC
-LOCAL_LDLIBS := -llog -latomic
-LOCAL_SHARED_LIBRARIES := gmp gmpxx
-LOCAL_STATIC_LIBRARIES := crypto ssl
+LOCAL_LDLIBS := -llog -latomic -Wl,--no-warn-shared-textrel
+LOCAL_STATIC_LIBRARIES := crypto ssl gmpxx gmp
 include $(BUILD_SHARED_LIBRARY)
